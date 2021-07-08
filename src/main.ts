9fbf28041cce451d5aede97ab421bc6c936a4a2c
Pasechnik Vladislav <develop.pvv@gmail.com>
2021-07-08 12:16:45 +0300

    Добавляет cors()

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { AuthGuard } from './auth/auth.guard';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalGuards(new AuthGuard());
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
